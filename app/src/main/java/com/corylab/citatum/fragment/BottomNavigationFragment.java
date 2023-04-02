6fbb9297d77016ffc5c1705dc842dc20769df1f9
linilinn <125601952+linilinn@users.noreply.github.com>
2023-04-02 21:42:52 +0300

    add layouts: - add class and layout - add lines in dimens.xml and strings.xml - add title item for recyclerview

---
+++ app/src/main/java/com/corylab/citatum/fragment/BottomNavigationFragment.java
@@ -0,0 +1,12 @@
+package com.corylab.citatum.fragment;
+
+import androidx.fragment.app.Fragment;
+
+import com.corylab.citatum.R;
+
+public class BottomNavigationFragment extends Fragment {
+
+    public BottomNavigationFragment() {
+        super(R.layout.bottom_navigation);
+    }
+}
---
+++ app/src/main/java/com/corylab/citatum/fragment/QuoteCreateFragment.java
@@ -0,0 +1,12 @@
+package com.corylab.citatum.fragment;
+
+import androidx.fragment.app.Fragment;
+
+import com.corylab.citatum.R;
+
+public class QuoteCreateFragment extends Fragment {
+
+    public QuoteCreateFragment() {
+        super(R.layout.quote_create);
+    }
+}
